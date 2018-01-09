3500b744635ef69e81dfb1c39bf22ab34f792366
Dariusz Aniszewski <dariusz.aniszewski@polidea.com>
2018-01-09 12:04:28 +0100

    use custom Perfkit

--- .test-infra/jenkins/common_job_properties.groovy
+++ .test-infra/jenkins/common_job_properties.groovy
@@ -263,7 +263,7 @@ class common_job_properties {
       // Clean up environment.
       shell('rm -rf PerfKitBenchmarker')
       // Clone appropriate perfkit branch
-      shell('git clone https://github.com/GoogleCloudPlatform/PerfKitBenchmarker.git')
+      shell('git clone -b jenkins https://github.com/DariuszAniszewski/PerfKitBenchmarker.git')
       // Install Perfkit benchmark requirements.
       shell('pip install --user -r PerfKitBenchmarker/requirements.txt')
       // Install job requirements for Python SDK.
