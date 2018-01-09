a2685c4b8c019afc74f753ed7061890e29d88c82
Dariusz Aniszewski <dariusz.aniszewski@polidea.com>
2018-01-09 11:34:00 +0100

    use latest PerfKit

--- .test-infra/jenkins/common_job_properties.groovy
+++ .test-infra/jenkins/common_job_properties.groovy
@@ -263,7 +263,7 @@ class common_job_properties {
       // Clean up environment.
       shell('rm -rf PerfKitBenchmarker')
       // Clone appropriate perfkit branch
-      shell('git clone -b verbose https://github.com/DariuszAniszewski/PerfKitBenchmarker.git')
+      shell('git clone https://github.com/GoogleCloudPlatform/PerfKitBenchmarker.git')
       // Install Perfkit benchmark requirements.
       shell('pip install --user -r PerfKitBenchmarker/requirements.txt')
       // Install job requirements for Python SDK.
